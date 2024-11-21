5f3a705ce0d23921849bfd54b15d596894cd45d8
AngelIsaiSzr <angelmoreno152000@gmail.com>
2024-11-21 16:10:13 -0600

    Se corrigieron estilos

--- assets/js/login.js
+++ assets/js/login.js
@@ -13,7 +13,6 @@ if (localStorage.getItem("email")) {
 const loginForm = document.getElementById("loginForm");
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
-const errorMessage = document.getElementById("errorMessage");
 
 // Manejar el evento de envío del formulario
 loginForm.addEventListener("submit", (event) => {
@@ -24,14 +23,32 @@ loginForm.addEventListener("submit", (event) => {
 
     // Comparar las credenciales ingresadas con las válidas
     if (email === validCredentials.email && password === validCredentials.password) {
+        // Guardar los datos en localStorage
         localStorage.setItem("email", email); // Guardar email en localStorage
         localStorage.setItem("cart", JSON.stringify([])); // Inicializar carrito como array vacío
         localStorage.setItem("quantity", "0"); // Inicializar cantidad en 0
-        window.location.href = "index.html"; // Redirigir al index
+
+        // SweetAlert para inicio de sesión exitoso
+        Swal.fire({
+            title: "¡Inicio de Sesión Exitoso!",
+            text: `Bienvenido, ${email}`,
+            icon: "success",
+            confirmButtonColor: "#198754",
+        }).then(() => {
+            // Redirigir al index después de cerrar el modal
+            window.location.href = "index.html";
+        });
     } else {
-        // Mostrar mensaje de error
-        errorMessage.style.display = "block";
-        emailInput.value = ""; // Borrar valores
+        // SweetAlert para error en el inicio de sesión
+        Swal.fire({
+            title: "Error",
+            text: "Correo electrónico o contraseña no válidos. Inténtalo de nuevo.",
+            icon: "error",
+            confirmButtonColor: "#d33",
+        });
+
+        // Limpiar los valores del formulario
+        emailInput.value = "";
         passwordInput.value = "";
     }
 });
